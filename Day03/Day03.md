cd04df80f0a5a6bcd38bc5cc7fb2ffb868996582
shashank <shekhar1245shashank@gmail.com>
2021-05-25 23:53:01 +0530

    Day-2 5 question attempted

---
+++ Day03/Day03.md
@@ -0,0 +1,15 @@
+# Day-03
+
+```
+ Q1 : find duplicate in an array of
+      N+1 Integers.
+
+ Q2 : Merge 2 sorted arrays without 
+      using Extra space.
+
+ Q3 : Kadane's Algo [V.V.V.V.V IMP]
+
+ Q4 : Merge Intervals
+
+ Q5 : Next Permutation
+```
---
+++ Day03/Q1.cpp
@@ -0,0 +1,34 @@
+//find Duplicates in array : 
+
+#include<iostream>
+using namespace std;
+
+//Brute force
+bool find_duplicates(int array[],int size){
+    for(int idx=0;idx<size;idx++){
+        int last_element=array[idx];
+        for(int idx_next=(idx+1);idx_next<size;idx++){
+            if(array[idx_next]==last_element){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int size;
+    cin>>size;
+
+    int array[size];
+    for(int idx=0;idx<size;idx++){
+        cin>>array[idx];
+    }
+
+    
+    cout<<"Duplicates in array of size N+1 : "<<find_duplicates(array,size)<<endl;
+
+    return 0;
+}
+
+//It had attempted this question only due to vaccination : sad

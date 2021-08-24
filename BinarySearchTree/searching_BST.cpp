a5df6b25ed83cfd0844e2ed536e5a95b593f7bf8
shashank <shekhar1245shashank@gmail.com>
2021-08-25 00:09:00 +0530

    Build and some basic operation on BST

---
+++ BinarySearchTree/searching_BST.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+//node of BST
+class node{
+public:
+    int data;
+    node*left;
+    node*right;
+
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+//Accepts the old root node & data and returns the new root node 
+node* insertInBST(node *root,int data){
+    //Base Case
+    if(root==NULL){
+        return new node(data);
+    }
+    //Rec Case - Insert in the Subtree and Update Pointers
+    // node->data <= root->data  goes left side.  
+    if(data<=root->data){
+        root->left = insertInBST(root->left,data);
+    }
+
+    // node->data > root->data  goes right side.  
+    else{
+        root->right = insertInBST(root->right,data);
+    }
+    return root;
+}
+
+node* build(){
+    //Read a list of numbers till -1 and also these numbers will be inserted into BST
+    int d;
+    cin>>d;
+
+    node*root = NULL;
+
+    while(d!=-1){
+        root = insertInBST(root,d);
+        cin>>d;
+    }
+    return root;
+}
+
+//Print the BST Level By Level
+void bfs(node *root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* f = q.front();
+        if(f==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<f->data<<",";
+            q.pop();
+
+            if(f->left){
+                q.push(f->left);
+            }
+            if(f->right){
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
+
+//Inorder Print : Always sorted.
+void inorder(node*root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<",";
+    inorder(root->right);
+}
+
+bool search(node* root,int key){
+    if(root==NULL){
+        return false;
+    }
+
+    if(root->data==key){
+        return true;
+    }
+
+    if(key<=root->data){
+        return search(root->left,key);
+    }
+    else{
+        return search(root->right,key);
+    }
+}
+
+
+int main(){ 
+    node*root = build();
+    inorder(root);
+    cout<<endl;
+    bfs(root);
+
+    int key;
+    cout<<"Key : "; 
+    cin>>key;
+
+    bool ans=search(root,key);
+
+    if(ans){
+        cout<<"Present";
+    }
+    else{
+        cout<<"Not present";
+    }
+return 0;
+}
